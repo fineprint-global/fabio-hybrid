c3937f17b99076b7406f67a95c1975059e910ece
Paul Höfle <paul.hoefle@wu.ac.at>
2026-08-05 09:28:07 +0000

    Implemented Gloira integration

--- 1_B_quadrant.R
+++ 1_B_quadrant.R
@@ -30,20 +30,35 @@ if (hybrid_model == "exio") {
 }
 
 if (hybrid_model == "gloria") {
-  
+  # Sort by FABIO area code so row order is canonical
   conc <- conc[order(conc$FABIO_area_code), ]
-  conc$GLORIA_region_index <- 1:nrow(conc)
-  # conc$EXIOBASE_code[is.na(conc$EXIOBASE_code)] <- 50 # Move NAs to extra column to drop, allocate at some point
+  
+  # Build sequential FABIO row index (repeats for multi-mapped countries)
+  # Each unique FABIO country gets one row in Cou
+  conc$FABIO_code <- match(conc$FABIO_area_code, 
+                           sort(unique(conc$FABIO_area_code)))
+  
+  sup = sup[complete.cases(sup), ]
+  use = use[complete.cases(use), ]
   
   Sup <- as_matrix(sup)
   Use <- as_matrix(use)
-  Cou <- sparseMatrix(i = conc$FABIO_area_code, j = conc$GLORIA_region_index) * 1
+  
+  n_fabio  <- length(unique(conc$FABIO_area_code))   # 181
+  n_gloria <- length(unique(conc$GLORIA_region_code))  # 161
+  
+  # the sparseMatrix constructor inserts value x at position i/y. If there are multiple, it sums (in this case, all multi-mappings are 0 in conc per default, but it would support percentages)
+  Cou <- sparseMatrix(i    = conc$FABIO_code,
+                      j    = conc$GLORIA_Lfd_Nr,
+                      x    = conc$allocation_share,
+                      dims = c(n_fabio, n_gloria))
 }
 
 # Function to create the hybrid part for a certain year
-hybridise <- function(year, Sup, Use, Cou, Y_all) {
+hybridise <- function(year, Sup, Use, Cou, Y_all, hybrid_model) {
   
   require(Matrix) # Necessary for forked processes
+  require(qs2)
 
   # Read EXIOBASE Z
   if (hybrid_model == "exio") {
@@ -60,16 +75,16 @@ hybridise <- function(year, Sup, Use, Cou, Y_all) {
     for(i in 1:49) {
       tmp <- Matrix(0, nrow = 200, ncol = 200)
       for(j in 1:49)
-        tmp <- tmp + Z[(1 + 200 * (j - 1)):(200 * j), 
+        tmp <- tmp + Z[(1 + 200 * (j - 1)):(200 * j),
                        (1 + 200 * (i - 1)):(200 * i)]
-      Tec[[i]] <- tmp 
+      Tec[[i]] <- tmp
     }
     
   }
   
   # Read Gloria Z
   if (hybrid_model == "gloria") {
-    Z <- qs_read(paste0("/mnt/nfs_fineprint/tmp/gloria/v060-compiled/IOTs_basic_prices/Z/Z_", year, ".qs2")) 
+    Z <- qs_read(paste0("/mnt/nfs_fineprint/tmp/gloria/v060-compiled/IOTs_basic_prices/Z/Z_", year, ".qs2"))
   
     # Gloria dimensions
     n_regions <- 164
@@ -78,27 +93,25 @@ hybridise <- function(year, Sup, Use, Cou, Y_all) {
     stopifnot(nrow(Z) == n_regions * n_sectors)
     
     # Calculate Tech matrices for the 164 Gloria regions
+    print(paste0(Sys.time(), " : Calculate Tech matrices for the 164 Gloria regions"))
     Tec <- vector("list", n_regions)
     for (i in 1:n_regions) {
       tmp <- Matrix(0, nrow = n_sectors, ncol = n_sectors)
       for (j in 1:n_regions)
-        tmp <- tmp + Z[(1 + n_sectors * (j - 1)):(n_sectors * j), 
+        tmp <- tmp + Z[(1 + n_sectors * (j - 1)):(n_sectors * j),
                        (1 + n_sectors * (i - 1)):(n_sectors * i)]
       Tec[[i]] <- tmp
     }
   
-    # Tec processing depends on matrix dimensions, this could even be separated out as global vars.
     # Z for EXIOBASE has dims 9800 * 9800, which is porcessed in 200*200 (sector x sector) blocks
     # Z for Gloria   has dims 19680 19680
     # 200 sectors * 49 regions = 9,800
-    # for Gloria, it would be 120 sectors * 164 regions = 19680
-    # in FABIO, there are 180 regions * 123 sectors (so some splitting logic would map presumably)
+    # for Gloria, there are 120 sectors * 164 regions = 19680
   }
   
   
   
-  # Y comes from FABIO, can thus stay the same; 192 seems to be the old FABIO country count (so division is to normalize it to 1, needs to depend on version)
-  
+  # TODO: Y comes from FABIO, can thus stay the same; 192 is a FABIO country count (so division is to normalize it to 1, needs to depend on version)
   
   # read FABIO Y
   Y <- Y_all[[as.character(year)]]
@@ -107,63 +120,98 @@ hybridise <- function(year, Sup, Use, Cou, Y_all) {
   Oth <- Y[, grep("other$", colnames(Y))]
   rm(Y, Z)
   
+  print(paste0(Sys.time(), " : Match FABIO countries with EXIOBASE countries and restructure the Other use matrix"))
   # Match FABIO countries with EXIOBASE countries and restructure the Other use matrix
-  Oth <- Oth %*% Cou
-  nprod <- nrow(Oth) / 192
+  # TODO: the 192/181 regions, 49 regions, 200 sectors, etc.. here need to be universally replaced to be compatible with older versions
   
-  # Create matrix for sector matching
-  T <- vector("list", 49)
-  for(i in 1:49) {
-    T[[i]] <- Sup[1:nprod,] %*% Tec[[i]] * Use[1:nprod,]
-    T[[i]] <- T[[i]] / rowSums(T[[i]])
-    T[[i]][is.na(T[[i]])] <- 0
+  Oth <- Oth %*% Cou
+  
+  if (vers %in% c("v1.1", "v1.2")) {
+    nprod <- nrow(Oth) / 192
+  } else if (vers == "v2") {
+    nprod <- nrow(Oth) / 181
+  } else {
+    stop("Invalid version code given")
   }
   
-  # Compute the hybrid part from Oth and T
-  B <- Matrix(0, nrow = 192 * nprod, ncol = 49 * 200, sparse = TRUE)
-  for(i in 1:49) {
-    B[, (1 + 200 * (i - 1)):(200 * i)] <-
-      do.call(rbind, replicate(192, T[[i]], simplify = FALSE)) * Oth[, i]
+  if (hybrid_model == "exio") {
+  # Create matrix for sector matching
+    T <- vector("list", 49)
+    for(i in 1:49) {
+      T[[i]] <- Sup[1:nprod,] %*% Tec[[i]] * Use[1:nprod,]
+      T[[i]] <- T[[i]] / rowSums(T[[i]])
+      T[[i]][is.na(T[[i]])] <- 0
+    }
+    
+    # Compute the hybrid part from Oth and T
+    B <- Matrix(0, nrow = 192 * nprod, ncol = 49 * 200, sparse = TRUE)
+    for(i in 1:49) {
+      B[, (1 + 200 * (i - 1)):(200 * i)] <-
+        do.call(rbind, replicate(192, T[[i]], simplify = FALSE)) * Oth[, i]
+    }
+  }
+  
+  if (hybrid_model == "gloria") {
+    
+    # Gloria dimensions
+    n_regions <- 164
+    n_sectors <- 120
+    
+    # Create matrix for sector matching
+    print(paste0(Sys.time(), " : Create matrix for sector matching"))
+    T <- vector("list", n_regions)
+    for(i in 1:n_regions) {
+      T[[i]] <- Sup[1:nprod,] %*% Tec[[i]] * Use[1:nprod,]
+      T[[i]] <- T[[i]] / rowSums(T[[i]])
+      T[[i]][is.na(T[[i]])] <- 0
+    }
+    
+    # Compute the hybrid part from Oth and T
+    print(paste0(Sys.time(), " : Compute the hybrid part from Oth and T"))
+    B <- Matrix(0, nrow = 181 * nprod, ncol = n_regions * n_sectors, sparse = TRUE)
+    for(i in 1:n_regions) {
+      B[, (1 + n_sectors * (i - 1)):(n_sectors * i)] <-
+        do.call(rbind, replicate(181, T[[i]], simplify = FALSE)) * Oth[, i]
+    }
   }
   
   B
 }
 
-
-
-
 # Execute -----------------------------------------------------------------
 
 # Select fabio version or run loop
 versions <- c("", "losses/")
-version <- versions[1]
+versions <- versions[2]
 
 for(version in versions){
   Y_all <- readRDS(paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/", version, "Y.rds"))
   
   # Setup to process in parallel
-  n_cores <- parallel::detectCores() - 2
-  cl <- parallel::makeCluster(n_cores)
+  # n_cores <- parallel::detectCores() - 2
+  # cl <- parallel::makeCluster(n_cores)
   
   # Years to calculate hybridised FABIO for
-  years <- 1986:2020
+  # years <- 1986:2020
+  years <- 2010:2023
   
-  output <- parallel::parLapply(cl, years, hybridise, Sup, Use, Cou, Y_all)
-  
-  for(i in seq_along(output)) {
-    saveRDS(output[[i]], 
-            paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", version, years[[i]], "_B.rds"))
-  }
-  
-  parallel::stopCluster(cl)
-  
-  rm(cl, Y_all, n_cores, years, output); gc()
-  
-  # # Alternatively run a loop
-  # for(year in years){
-  #   saveRDS(hybridise(year, Sup, Use, Cou, Y_all), 
-  #           paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", version, year, "_B.rds"))
+  # output <- parallel::parLapply(cl, years, hybridise, Sup, Use, Cou, Y_all, hybrid_model)
+  # 
+  # for(i in seq_along(output)) {
+  #   saveRDS(output[[i]], 
+  #           paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", version, years[[i]], "_B.rds"))
   # }
+  
+  # parallel::stopCluster(cl)
+  
+  # rm(cl, Y_all, n_cores, years, output); gc()
+  
+  # Alternatively run a loop
+  for(year in years){
+    print(paste0("processing ", year))
+    saveRDS(hybridise(year, Sup, Use, Cou, Y_all, hybrid_model),
+            paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", hybrid_model, "/", version, year, "_B.rds"))
+  }
 }
 
 
--- 2_invert_B.R
+++ 2_invert_B.R
@@ -6,45 +6,112 @@
 # for C = 0 -->  B^-1 = -A^-1 BD^-1
 
 library(Matrix)
+library(qs2)
 
-vers <- "v1.2" # "v1.1"
-year=2013
-years <- 1986:2020
+vers <- "v2" # "v1.1", v1.2
+hybrid_model <- "gloria" #"exio"
+years <- 2010:2023
 versions <- c("","losses/")
-version="losses/"
+versions = versions[1]
+# version = versions[1] # TODO: rename, is not ideal for debugging
 
 for(version in versions){
   for(year in years){
     print(paste0(version,year))
+    print(Sys.time())
     
-    if(year<1995){
-      load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/1995_L.RData"))
-      load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/1995_x.RData"))
-    } else if(year>2016) {
-      load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/2016_L.RData"))
-      load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/2016_x.RData"))
-    } else {
-      load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/", year, "_x.RData"))
-      load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/", year, "_L.RData"))
+    if (hybrid_model == "exio") {
+      if(year<1995){
+        load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/1995_L.RData"))
+        load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/1995_x.RData"))
+      } else if(year>2016) {
+        load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/2016_L.RData"))
+        load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/2016_x.RData"))
+      } else {
+        load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/", year, "_x.RData"))
+        load(paste0("/mnt/nfs_fineprint/tmp/exiobase/pxp/", year, "_L.RData"))
+      }
+      
+      D_inv <- L
+      rm(L); gc()
     }
     
-    D_inv <- L
-    rm(L); gc()
+    if (hybrid_model == "gloria") {
+      D_inv <- qs_read(paste0("~/Hybrid-Conc/Fabio_Hybrid_Output/Gloria_L_Matrices/L_", year, ".qs2"))
+      x <- qs_read(paste0("/mnt/nfs_fineprint/tmp/gloria/v060-compiled/IOTs_basic_prices/X/X_", year, ".qs2"))
+    }
     
-    B <- readRDS(paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", version, year, "_B.rds"))
-    B <- t(t(B)/x)
-    B[!is.finite(B)] <- 0
-    B[B<0] <- 0
-    B <- 0-B
+    B <- readRDS(paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", hybrid_model, "/", version, year, "_B.rds"))
     
+    # Ensure the matrix is sparse
+    # D_inv <- as(D_inv, "sparseMatrix")
+    
+    # print("Division")
+    # print(Sys.time())
+    # B <- t(t(B)/x)
+    # 
+    # print("Filtering")
+    # print(Sys.time())
+    # B[!is.finite(B)] <- 0
+    # B[B<0] <- 0
+    # B <- 0-B
+    
+    # sparse-optimized version of the calulation above
+    print("Division")
+    print(Sys.time())
+    
+    # Column-scale B by 1/x, sparse-safe
+    x_inv <- 1 / x
+    x_inv[!is.finite(x_inv)] <- 0
+    B <- B %*% Diagonal(x = x_inv)
+    
+    # Verify sparsity was preserved
+    stopifnot(inherits(B, "CsparseMatrix"))
+    
+    print("Filtering")
+    print(Sys.time())
+    
+    # Operate on nonzero values only (sparse-safe)
+    B@x[!is.finite(B@x)] <- 0
+    B@x[B@x < 0] <- 0
+    B <- drop0(B)
+    
+    # Negate (sparse-safe: just flips signs of @x)
+    B <- -B
+    
+    print("Load A inv")
+    print(Sys.time())
     A_inv <- readRDS(paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/", version, year, "_L_mass.rds"))
-    B_inv <- -A_inv %*% B %*% D_inv
-    saveRDS(B_inv, paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", version, year, "_B_inv_mass.rds"))
     
+    print("second calculation (mass)")
+    print(Sys.time())
+    B_inv <- -A_inv %*% B %*% D_inv
+    
+    rm(A_inv)
+    gc(verbose = FALSE)
+    
+    print("saving")
+    print(Sys.time())
+    saveRDS(B_inv, paste0("~/Hybrid-Conc/Fabio_Hybrid_Output/Hybrid_Inv_Matrices/",vers,"/hybrid/", hybrid_model, "/", version, year, "_B_inv_mass.rds"))
+    
+    rm(B_inv)
+    gc(verbose = FALSE)
+    
+    print("third calculation (value)")
+    print(Sys.time())
     A_inv <- readRDS(paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/", version, year, "_L_value.rds"))
     B_inv <- -A_inv %*% B %*% D_inv
-    saveRDS(B_inv, paste0("/mnt/nfs_fineprint/tmp/fabio/",vers,"/hybrid/", version, year, "_B_inv_value.rds"))
     
+    print("saving")
+    print(Sys.time())
+    saveRDS(B_inv, paste0("~/Hybrid-Conc/Fabio_Hybrid_Output/Hybrid_Inv_Matrices/",vers, "/hybrid/", hybrid_model, "/", version, year, "_B_inv_value.rds"))
+    
+    print("Finished:")
+    print(paste0(version,year))
+    print(Sys.time())
+    
+    rm(B_inv, A_inv, B, D_inv)
+    gc(verbose = FALSE)
   }
 }
 
